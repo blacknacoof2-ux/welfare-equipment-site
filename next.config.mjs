3263f3cb248c57c960889395568a8ea787a532fb
blacknacoof2-ux <blacknacoof2@gmail.com>
2026-09-12 16:50:26 +0900

    chore: require locally controlled or authorized product images

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**.eroum.co.kr' },
-      { protocol: 'https', hostname: '**.eroumcare.com' },
-      { protocol: 'https', hostname: '**.nhis.or.kr' }
-    ]
-  }
+  reactStrictMode: true
 };
 
 export default nextConfig;
