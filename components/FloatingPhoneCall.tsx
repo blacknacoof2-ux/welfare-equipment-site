a9dbb65f065b3b368c73121c5030abb7b1610ec6
blacknacoof2-ux <blacknacoof2@gmail.com>
2026-09-15 15:00:11 +0900

    Add persistent purchase inquiry phone CTA

---
+++ components/FloatingPhoneCall.tsx
@@ -0,0 +1,10 @@
+export default function FloatingPhoneCall() {
+  return (
+    <aside className="floating-phone-call" aria-label="구매 및 문의 전화">
+      <span>구매 및 문의</span>
+      <a href="tel:0319753335" aria-label="아톰케어랩 구매 및 문의 031-975-3335로 전화하기">
+        <b>☎</b> 031-975-3335
+      </a>
+    </aside>
+  );
+}
